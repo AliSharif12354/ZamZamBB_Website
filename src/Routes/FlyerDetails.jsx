ff73e353ca60f900239676181bbf19e89b3fff95
Ali Hassan Sharif <71347625+AliSharif12354@users.noreply.github.com>
2023-05-05 13:07:35 -0400

    FlyerDetails delete and archive running
    App.jsx has some problems with flyers tho, will look into that

--- src/Routes/FlyerDetails.jsx
+++ src/Routes/FlyerDetails.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { db } from '../Firebase';
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import { doc, getDoc } from 'firebase/firestore';
+import { doc, getDoc, updateDoc, deleteDoc } from 'firebase/firestore';
 import Flyer from '../Components/Flyer';
 import Navbar_V2 from '../Components/Navbar_V2';
 import Footer from '../Components/Footer'
 import "../Styles/FlyerDetails.css"
+
 export default function FlyerDetails(props) {
 
     const { flyerID } = useParams();
@@ -29,39 +30,59 @@ export default function FlyerDetails(props) {
         return <div>Loading...</div>;
     }
 
-    function handleFormSubmit(e) {
-        e.preventDefault()
-        console.log(e)
+    async function handleFormSubmit(e) {
+        e.preventDefault();
+        const deleteValue = e.target.delete.value;
+        const archiveValue = e.target.archive.value;
+        const docRef = doc(db, "flyers", flyerID);
+
+        if (deleteValue === "yes") { //Delete flyer document
+            try {
+                await deleteDoc(docRef);
+                console.log("Flyer document successfully deleted!");
+            } catch (error) {
+                console.error("Error deleting flyer document: ", error);
+            }
+        } else { //Update archive status
+            try {
+                await updateDoc(docRef, {
+                    archive: (archiveValue === "yes")
+                });
+                console.log("Flyer document successfully updated!");
+            } catch (error) {
+                console.error("Error updating flyer document: ", error);
+            }
+        }
     }
     return (
         <div className='content'>
-            
+
             <Navbar_V2 />
             <h1>Edit Flyer</h1>
             {console.log(flyerData)}
             <Flyer logo={flyerData.imgSrc} />
             <form onSubmit={handleFormSubmit}>
-                <br/>
+                <br />
                 <div>
                     <label>
-                    <p className='inputFlyer'>Delete Flyer? </p>
-                        <select style={{minHeight: "25px"}} className='inputFlyer' name="Delete" id="delete">
+                        <p className='inputFlyer'>Delete Flyer? </p>
+                        <select style={{ minHeight: "25px" }} className='inputFlyer' name="Delete" id="delete">
                             <option value="no">No</option>
                             <option value="yes">Yes</option>
                         </select>
                     </label>
                 </div>
-                <br/>
+                <br />
                 <div>
                     <label>
                         <p className='inputFlyer'>Archive status: (Invisible or not)</p>
-                        <select style={{minHeight: "25px"}} name="Archive" id="archive">
+                        <select style={{ minHeight: "25px" }} name="Archive" id="archive">s
                             <option value="no">No</option>
                             <option value="yes">Yes</option>
                         </select>
                     </label>
                 </div>
-                <br/>   
+                <br />
                 <button type="submit">Submit</button>
             </form>
             <Footer />
