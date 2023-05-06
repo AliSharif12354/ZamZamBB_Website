1a4127ecf386dd56d79ccc22731ba3093f3efad6
moeSeneca <123316379+moeSeneca@users.noreply.github.com>
2023-05-06 14:12:56 -0400

    small

--- src/Routes/EditProductSpecific.jsx
+++ src/Routes/EditProductSpecific.jsx
@@ -67,112 +67,117 @@ export default function EditProductSpecific(props) {
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(product)
-      };
+    };
 
-      const handleImageChange = (e) => {
+    const handleImageChange = (e) => {
         // ...
-      };
+    };
 
-      function handleCloseModal() {
+    function handleCloseModal() {
         setShowModal(false);
         setIsCompleted(false);
     }
 
 
-  
+    if (currentUser) {
 
-    out =
-        <>
-            <Navbar_V2 />
-            <div className='formContainer'>
-                <form onSubmit={handleSubmit}>
-                    <label className='formItem' htmlFor='image'>Image:</label>
-                    <input type='file' id='image' onChange={handleImageChange} required multiple />
-                    <br />
-                    <br />
+        console.log(product)
+        console.log(productID)
 
-                    <label className='formItem' htmlFor='name'>Name:</label>
-                    <input type='text' id='name' maxLength={30} value={name} onChange={(e) => setName(e.target.value)} required />
-                    <br />
-                    <br />
+        out =
+            <>
+                <Navbar_V2 />
+                <div className='formContainer'>
+                    <form onSubmit={handleSubmit}>
+                        <label className='formItem' htmlFor='image'>Image:</label>
+                        <input type='file' id='image' onChange={handleImageChange} required multiple />
+                        <br />
+                        <br />
 
-                    <label className='formItem' htmlFor='description'>Description:</label>
-                    <textarea id='description' style={{ width: '300px', height: '100px' }} value={description} onChange={(e) => setDescription(e.target.value)} required />
-                    <br />
-                    <br />
+                        <label className='formItem' htmlFor='name'>Name:</label>
+                        <input type='text' id='name' maxLength={30} value={name} onChange={(e) => setName(e.target.value)} required />
+                        <br />
+                        <br />
 
-                    <label className='formItem' htmlFor='price'>Price:</label>
-                    <input type='number' id='price' value={price} onChange={(e) => setPrice(e.target.value)} required />
-                    <br />
-                    <br />
+                        <label className='formItem' htmlFor='description'>Description:</label>
+                        <textarea id='description' style={{ width: '300px', height: '100px' }} value={description} onChange={(e) => setDescription(e.target.value)} required />
+                        <br />
+                        <br />
 
-                    <label className='formItem'><u>In Stock?:</u></label>
-                    <input type='radio' id='inStockTrue' name='inStock' value={true} onChange={(e) => setInStock(e.target.value)} required />
-                    {'   '}
-                    <label htmlFor='inStockTrue'>Yes</label>
-                    <br />
-                    {'   '}
-                    <input type='radio' id='inStockFalse' name='inStock' value={false} onChange={(e) => setInStock(e.target.value)} />
-                    {'   '}
-                    <label htmlFor='inStockFalse'> No </label>
-                    <br />
-                    <br />
+                        <label className='formItem' htmlFor='price'>Price:</label>
+                        <input type='number' id='price' value={price} onChange={(e) => setPrice(e.target.value)} required />
+                        <br />
+                        <br />
 
-                    <label className='formItem'>Is Product A&nbsp;<u>Luggage?</u> :</label>
-                    <input type='radio' id='isLuggageTrue' name='isLuggage' value={true} onChange={(e) => setIsLuggage(e.target.value)} required />
-                    {'   '}
-                    <label htmlFor='isLuggageTrue'>Yes</label>
-                    <br />
-                    {'   '}
-                    <input type='radio' id='isLuggageFalse' name='isLuggage' value={false} onChange={(e) => setIsLuggage(e.target.value)} />
-                    {'   '}
-                    <label htmlFor='isLuggageFalse'>No</label>
-                    <br />
-                    <br />
+                        <label className='formItem'><u>In Stock?:</u></label>
+                        <input type='radio' id='inStockTrue' name='inStock' value={true} onChange={(e) => setInStock(e.target.value)} required />
+                        {'   '}
+                        <label htmlFor='inStockTrue'>Yes</label>
+                        <br />
+                        {'   '}
+                        <input type='radio' id='inStockFalse' name='inStock' value={false} onChange={(e) => setInStock(e.target.value)} />
+                        {'   '}
+                        <label htmlFor='inStockFalse'> No </label>
+                        <br />
+                        <br />
 
-                    <label className='formItem'>Is Product A&nbsp;<u>Clothing?:</u> </label>
-                    <input type='radio' id='isClothingTrue' name='isClothing' value={true} onChange={(e) => setIsClothing(e.target.value)} required />
-                    {'   '}
-                    <label htmlFor='isClothingTrue'>Yes</label>
-                    <br />
-                    {'   '}
-                    <input type='radio' id='isClothingFalse' name='isClothing' value={false} onChange={(e) => setIsClothing(e.target.value)} />
-                    {'   '}
-                    <label htmlFor='isClothingFalse'>No</label>
-                    <br />
-                    <br />
+                        <label className='formItem'>Is Product A&nbsp;<u>Luggage?</u> :</label>
+                        <input type='radio' id='isLuggageTrue' name='isLuggage' value={true} onChange={(e) => setIsLuggage(e.target.value)} required />
+                        {'   '}
+                        <label htmlFor='isLuggageTrue'>Yes</label>
+                        <br />
+                        {'   '}
+                        <input type='radio' id='isLuggageFalse' name='isLuggage' value={false} onChange={(e) => setIsLuggage(e.target.value)} />
+                        {'   '}
+                        <label htmlFor='isLuggageFalse'>No</label>
+                        <br />
+                        <br />
 
-                    <label className='formItem'>Is Product A&nbsp;<u>Best Seller?:</u></label>
-                    <input type='radio' id='isBestSellerTrue' name='isBestSeller' value={true} onChange={(e) => setIsBestSeller(e.target.value)} required />
-                    {'   '}
-                    <label htmlFor='isBestSellerTrue'>Yes</label>
-                    <br />
-                    {'   '}
-                    <input type='radio' id='isBestSellerFalse' name='isBestSeller' value={false} onChange={(e) => setIsBestSeller(e.target.value)} />
-                    {'   '}
-                    <label htmlFor='isBestSellerFalse'>No</label>
-                    <br />
-                    <br />
-                    <Button className='success' type='submit'>Add Product</Button>
-                </form>
-                <Modal show={showModal} onHide={handleCloseModal} centered className='addmodal'>
-                    <Modal.Header closeButton>
-                        <Modal.Title>Success</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <p>The product has been added successfully!</p>
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <Link to='/editProducts'>
-                            <Button variant="secondary" onClick={handleCloseModal}>
-                                Close
-                            </Button>
-                        </Link>
-                    </Modal.Footer>
-                </Modal>
-            </div>
-            <Footer />
-        </>
+                        <label className='formItem'>Is Product A&nbsp;<u>Clothing?:</u> </label>
+                        <input type='radio' id='isClothingTrue' name='isClothing' value={true} onChange={(e) => setIsClothing(e.target.value)} required />
+                        {'   '}
+                        <label htmlFor='isClothingTrue'>Yes</label>
+                        <br />
+                        {'   '}
+                        <input type='radio' id='isClothingFalse' name='isClothing' value={false} onChange={(e) => setIsClothing(e.target.value)} />
+                        {'   '}
+                        <label htmlFor='isClothingFalse'>No</label>
+                        <br />
+                        <br />
+
+                        <label className='formItem'>Is Product A&nbsp;<u>Best Seller?:</u></label>
+                        <input type='radio' id='isBestSellerTrue' name='isBestSeller' value={true} onChange={(e) => setIsBestSeller(e.target.value)} required />
+                        {'   '}
+                        <label htmlFor='isBestSellerTrue'>Yes</label>
+                        <br />
+                        {'   '}
+                        <input type='radio' id='isBestSellerFalse' name='isBestSeller' value={false} onChange={(e) => setIsBestSeller(e.target.value)} />
+                        {'   '}
+                        <label htmlFor='isBestSellerFalse'>No</label>
+                        <br />
+                        <br />
+                        <Button className='success' type='submit'>Add Product</Button>
+                    </form>
+                    <Modal show={showModal} onHide={handleCloseModal} centered className='addmodal'>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Success</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <p>The product has been added successfully!</p>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Link to='/editProducts'>
+                                <Button variant="secondary" onClick={handleCloseModal}>
+                                    Close
+                                </Button>
+                            </Link>
+                        </Modal.Footer>
+                    </Modal>
+                </div>
+                <Footer />
+            </>
+
+    }
 
 
 
