278c3d928257c2023517040ba93c40d2ebadb5f9
Ali Hassan Sharif <71347625+AliSharif12354@users.noreply.github.com>
2023-05-11 03:32:57 -0400

    New try, might have to revert

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './Routes/App'
-import SignIn from './Routes/SignIn'
-import Products from './Routes/Products'
-import AdminRoute from './Routes/AdminRoute'
-import EditProductsRoute from './Routes/EditProductsRoute'
-import EditFlyerRoute from './Routes/EditFlyerRoute'
-import AddProductRoute from './Routes/AddProductRoute'
-import AddFlyerRoute from './Routes/AddFlyerRoute'
-import FlyerDetails from './Routes/FlyerDetails'
-import EditProductSpecific from './Routes/EditProductSpecific'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './Routes/App';
+import SignIn from './Routes/SignIn';
+import Products from './Routes/Products';
+import AdminRoute from './Routes/AdminRoute';
+import EditProductsRoute from './Routes/EditProductsRoute';
+import EditFlyerRoute from './Routes/EditFlyerRoute';
+import AddProductRoute from './Routes/AddProductRoute';
+import AddFlyerRoute from './Routes/AddFlyerRoute';
+import FlyerDetails from './Routes/FlyerDetails';
+import EditProductSpecific from './Routes/EditProductSpecific';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 const router = (
-  <Routes>
-    <Route path="/" element={<App />} />
-    <Route path="/signinadmin" element={<SignIn />} />
-    <Route path="/products" element={<Products />} />
-    <Route path="/adminRoute" element={<AdminRoute />} />
-    <Route path="/editFlyers" element={<EditFlyerRoute />} />
-    <Route path="/editProducts" element={<EditProductsRoute />} />
-    <Route path="/addProduct" element={<AddProductRoute />} />
-    <Route path="/flyerDetails/:flyerID" element={<FlyerDetails />} />
-    <Route path="/editProduct/:productID" element={<EditProductSpecific />} />
-    <Route path="/addFlyer" element={<AddFlyerRoute />} />
-  </Routes>
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/signinadmin" element={<SignIn />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/adminRoute" element={<AdminRoute />} />
+      <Route path="/editFlyers" element={<EditFlyerRoute />} />
+      <Route path="/editProducts" element={<EditProductsRoute />} />
+      <Route path="/addProduct" element={<AddProductRoute />} />
+      <Route path="/flyerDetails/:flyerID" element={<FlyerDetails />} />
+      <Route path="/editProduct/:productID" element={<EditProductSpecific />} />
+      <Route path="/addFlyer" element={<AddFlyerRoute />} />
+    </Routes>
+  </HashRouter>
 );
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <React.StrictMode>
     {router}
-  </BrowserRouter>
+  </React.StrictMode>
 );
